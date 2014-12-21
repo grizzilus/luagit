3bf246a5f8b85481b39e85dcf5829429986dbea3
Jeremy Engel <jengelsd@gmail.com>
2014-12-20 17:40:18 -0800

    Initial upload

---
+++ luagit.lua
@@ -0,0 +1,41 @@
+local trim = function(s)
+  return s:match'^%s*(.*%S)' or ''
+end
+
+local check_if_git_repo = function()
+  local f = io.popen('git rev-parse --is-inside-work-tree')
+  local temp = f:read("*all")
+  temp = trim(temp)
+  if temp == "true" then
+    return true
+  end
+  return false
+end
+
+local clean_lua_output = function(text)
+  local temp = trim(text)
+  temp = string.gsub(temp, "\n", "\\par")
+  return temp
+end
+
+git_commit_hash = function(parameter)
+  if check_if_git_repo() then
+    local f = io.popen('git rev-parse ' .. parameter)
+    local temp = f:read("*all")
+    return clean_lua_output(temp)
+  else
+    return "Not a git repository"
+  end
+end
+
+CURRENT_HEAD = git_commit_hash('HEAD')
+
+git_status = function()
+  if check_if_git_repo() then
+    local f = io.popen('git status -s')
+    local temp = f:read("*all")
+    return clean_lua_output(temp)
+  else
+    return "Not a git repository"
+  end
+end
